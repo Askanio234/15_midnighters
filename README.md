28ad9118728f39d597103984cc015a3ac1eeb58f
Artem Chernyaev <askanio234@gmail.com>
2017-03-11 16:12:26 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,44 @@
-# Night Owls Detector
+# Кто сова на девмане?
 
-[TODO. There will be project description]
+Скрипт запрашивает через API Девмана кто из юзеров сдавал задачи с 00.00 до 06.00 (в своей временной зоне) и выводит их имена в консоль
 
-# Project Goals
+# Как запустить
+Перед запуском необходимо установить зависимости из requirements.txt:
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+```#!bash
+pip install -r requirements.txt
+```
+## Запуск на Linux:
+
+```#!bash
+
+$ E:\Users\projects\15_midnighters>python seek_dev_nighters.py
+Совы на девмане:
+
+id297241917
+id15856034
+АртемЧерняев
+АндрейДенисов
+AnyaGoremykina
+beastrock
+id30126560
+МихаилКрылов
+id225397134
+KonstantinNaumov
+vladisgrig
+id331045184
+KamilLezwis
+albatrosicks
+НиколайТопович
+id20889227
+krilow
+moofo
+kavtaswin
+
+```
+
+## Запуск на Windows происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,62 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+from datetime import datetime
+import requests
+from pytz import timezone
+import pytz
 
-def get_midnighters():
-    pass
+DEVMAN_API = "https://devman.org/api/challenges/solution_attempts/"
+
+MIN_TIME = 0
+
+MAX_TIME = 6
+
+
+def get_number_of_pages(api):
+    request = requests.get(api)
+    request.encoding = "utf-8"
+    result = request.json()
+    return result["number_of_pages"]
+
+
+def load_attempts(api, number_of_pages):
+    attempts = []
+    for page in range(1, number_of_pages+1):
+        parameters = {"page": page}
+        request = requests.get(api, params=parameters)
+        request.encoding = "utf-8"
+        result = request.json()
+        attempts.extend(result["records"])
+    return attempts
+
+
+def convert_to_local_time(attempts):
+    attempts_in_local_time = []
+    for record in attempts:
+        if record["timestamp"] is not None:
+            user_time_zone = timezone(record["timezone"])
+            user_local_time = user_time_zone.localize(datetime.fromtimestamp(
+                                                        record["timestamp"]))
+            attempts_in_local_time.append({"username": record["username"],
+                                            "time": user_local_time})
+    return attempts_in_local_time
+
+
+def get_midnighters(attempts_in_local_time):
+    midnighters = set()
+    for record in attempts_in_local_time:
+        if MIN_TIME <= record["time"].time().hour <= MAX_TIME:
+            midnighters.add(record["username"])
+    return midnighters
+
+
+def print_midnighters(midnighters):
+    print("Совы на девмане:\n")
+    for midnighter in midnighters:
+        print("{}".format(midnighter))
+
 
 if __name__ == '__main__':
-  pass
+    number_of_pages = get_number_of_pages(DEVMAN_API)
+    attempts = load_attempts(DEVMAN_API, number_of_pages)
+    local_time_attempts = convert_to_local_time(attempts)
+    midnighters = get_midnighters(local_time_attempts)
+    print_midnighters(midnighters)
